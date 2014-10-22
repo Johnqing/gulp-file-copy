327dafc7fd856c4321d0a6a206fff8c678679764
刘卿 <csssnow@gmail.com>
2014-10-22 19:14:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs'),
+	path = require('path'),
 	through = require('through2'),
     gutil = require('gulp-util'),
     PLUGIN_NAME;
@@ -14,52 +15,49 @@ var stat = fs.stat;
  * @param{ String } 需要复制的目录
  * @param{ String } 复制到指定的目录
  */
-var copy = function( src, dst ){
-	// 读取目录中的所有文件/目录
-	fs.readdir( src, function( err, paths ){
+var copy = function(start, dst, _src){
+
+	var readable, writable;
+	var _dst;
+
+	_src = _src.replace(/\\/g, '/');
+	start = start.replace('./', '');
+	_dst = dst + _src.replace(start, '');
+
+	console.log(start, dst, _src);
+
+	stat( _src, function( err, st ){
 		if( err ){
 			throw err;
 		}
 
-		paths.forEach(function( path ){
-			var _src = src + '/' + path,
-				_dst = dst + '/' + path,
-				readable, writable;
-
-			stat( _src, function( err, st ){
-				if( err ){
-					throw err;
-				}
-
-				// 判断是否为文件
-				if( st.isFile() ){
-					// 创建读取流
-					readable = fs.createReadStream( _src );
-					// 创建写入流
-					writable = fs.createWriteStream( _dst );
-					// 通过管道来传输流
-					readable.pipe( writable );
-				}
-				// 如果是目录则递归调用自身
-				else if( st.isDirectory() ){
-					exists( _src, _dst, copy );
-				}
-			});
-		});
+		// 判断是否为文件
+		if(st.isFile()){
+			// 创建读取流
+			readable = fs.createReadStream( _src );
+			// 创建写入流
+			writable = fs.createWriteStream( _dst );
+			// 通过管道来传输流
+			readable.pipe( writable );
+		}
+		// 如果是目录则递归调用自身
+		else if( st.isDirectory() ){
+			exists( _src, _dst, copy );
+		}
 	});
 };
 
 // 在复制目录前需要判断该目录是否存在，不存在需要先创建目录
-var exists = function( src, dst, callback ){
+var exists = function( src, dst, callback, source){
 	fs.exists( dst, function( exists ){
 		// 已存在
 		if( exists ){
-			callback( src, dst );
+			callback( src, dst, source);
 		}
 		// 不存在
 		else{
 			fs.mkdir( dst, function(){
-				callback( src, dst );
+				callback( src, dst, source);
 			});
 		}
 	});
@@ -79,7 +77,9 @@ module.exports = function (dst, options) {
 			return cb();
 		}
 
-		exists(options.start, dst, copy);
+		var rel = path.relative(file.cwd, file.path);
+		exists(options.start, dst, copy, rel);
+		this.push(file);
 		cb();
 	});
 
